8d1d5d6043c95b5225d78341ab271bc44be0ad4f
Abin Biju <hmovies957@gmail.com>
2026-03-09 23:38:06 +0530

    job portal sqlite version

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -75,7 +75,10 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
